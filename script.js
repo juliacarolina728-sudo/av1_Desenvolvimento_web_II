214c22788a3cf7fbaaa196c520693faf97f9eeac
Julia Carolina Inácio <juliacarolina728@gmail.com>
2026-08-16 01:13:47 -0300

    adiciona cards

---
+++ script.js
@@ -0,0 +1,9 @@
+const modalOverlay = document.getElementById("modalOverlay");
+
+document.getElementById("btnAbrirModal").addEventListener("click", () => {
+  modalOverlay.classList.add("active");
+});
+
+document.getElementById("modalClose").addEventListener("click", () => {
+  modalOverlay.classList.remove("active");
+});
