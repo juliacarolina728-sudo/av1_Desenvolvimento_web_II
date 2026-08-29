bd8c30156174601b1152277ab8f350f4dde6db42
Julia Carolina Inácio <juliacarolina728@gmail.com>
2026-08-29 17:24:22 -0300

    feat ativ 6

---
+++ scripts/script6.js
@@ -0,0 +1,13 @@
+
+let comissao = [];
+
+const modalOverlay = document.getElementById("modalOverlay");
+
+document.getElementById("btnAbrirModal").addEventListener("click", () => {
+  modalOverlay.classList.add("active");
+});
+
+document.getElementById("modalClose").addEventListener("click", () => {
+  modalOverlay.classList.remove("active");
+});
+
